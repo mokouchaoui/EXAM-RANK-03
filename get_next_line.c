d64b92490b7835cc056e5cc9d7c6da1f0ce33e17
Mohamed Kouchaoui <mokoucha@student.42heilbronn.de>
2022-07-17 17:46:12 +0200

    start

--- get_next_line.c
+++ get_next_line.c
@@ -1,41 +1,7 @@
-#include <stdlib.h>
 #include <unistd.h>
-
-char *ft_strdup(char *s)
-{
-    int i = 0;
-    while (s[i])
-        i++;
-    char *t = malloc(i + 1);
-    i = 0;
-    while (s[i])
-    {
-        t[i] = s[i];
-        i++;
-    }
-    t[i] = 0;
-    return (t);
-}
-
-char *get_next_line(int fd)
-{
-    if (fd < 0)
-        return (NULL);
-	
-    char buff[1];
-    char line[1000000];
-    line[0] = 0;
-    int i = 0;
-    int n;
-	
-    while ((n = read(fd, buff, 1)) && n > 0)
-    {
-        line[i++] = buff[0];
-        line[i] = '\0';
-        if (buff[0] == '\n')
-            return (ft_strdup(line));
-    }
-    if (!line[0])
-        return (NULL);
-    return (ft_strdup(line));
+#include <stdlib.h>
+char *gnl(int fd) {
+    char *s = malloc(10000), *c = s;
+    while (read(fd, c, 1) > 0 && *c++ != '\n');
+    return c > s ? (*c = 0, s) : (free(s), NULL);
 }
